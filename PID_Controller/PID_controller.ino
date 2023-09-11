ce7674977ed8e318f3fddc3624841eb5e28ee585
MennatullahBasiony <mennatullahbasiony@gmail.com>
2023-09-11 20:53:09 +0300

    PID_controller.ino file updated

--- PID_Controller/PID_controller.ino
+++ PID_Controller/PID_controller.ino
@@ -1,36 +1,38 @@
+#include <PID_v1.h>
+
 #define Flow_Rate A0 
 #define OUTPUT_PIN 3
 
-double dt, last_time;  
-double integral, previous, output = 0;
 double kp, ki, kd;
 double Desired_FlowRate = 90.00;
 double FlowRate = 0;
+double current_FlowRate;
 
 void setup()
 {
   kp = 2 ; 
   ki = 0.1;
   kd = 0.01;
-  last_time = 0;
+
   Serial.begin(9600);
   pinMode(Flow_Rate,INPUT);
   analogWrite(OUTPUT_PIN, 0);
+
+  PID myPID(FlowRate, current_Rate, Desired_FlowRate, kp, ki, kd, DIRECT);
+  myPID.SetMode(AUTOMATIC);
   delay(10);
 }
 
 void loop()
 {
-  double now = millis();
-  dt = (now - last_time)/1000.00;
-  last_time = now;
 
   double actual = analogRead(Flow_Rate); //Read the feedback from the flow meter
   FlowRate = actual*60 / 1023 ; // Calculate the flow rate in CFM 
   double error = Desired_FlowRate - FlowRate; // Calculate the error signal 
-  output = pid(error); // Apply PID algorthim
 
-  analogWrite(OUTPUT_PIN, output); 
+  myPID.Compute();
+
+  analogWrite(OUTPUT_PIN, current_FlowRate); 
 
   // Desired_FlowRate VS FlowRate
   Serial.print(Desired_FlowRate);
@@ -41,19 +43,4 @@ void loop()
   Serial.println(error);
 
   delay(10);
-}
-// PID Calculation Function
-double pid(double error)
-{
-  // P Controller
-  double proportional = error;
-  // I Controller
-  integral += error * dt;
-  // D Controller
-  double derivative = (error - previous) / dt;
-  // Update Data
-  previous = error;
-  // PID Equation
-  double output = (kp * proportional) + (ki * integral) + (kd * derivative);
-  return output;
 }
